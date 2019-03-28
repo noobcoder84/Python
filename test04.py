9abf85f7a610f23d1f10566a0dffcd33a54d5ede
Sarmad Nashat <sarmad@sarosmbp.local>
2019-03-29 01:36:31 +0300

    First commit

---
+++ test04.py
@@ -0,0 +1,12 @@
+def main():
+
+    def getInt():
+        result = int(input("Enter number: "))
+        return result
+
+    num = getInt()
+    print(num)
+
+if __name__ == "__main__":
+    main()
+
---
+++ test09.py
@@ -0,0 +1,12 @@
+def main():
+
+    x = int(input("enter number: "))
+    if x%2==0:
+        print("square")
+    else:
+        print("see you")
+
+if __name__ == "__main__":
+    main()
+
+
