07c2041658c2afcd9226300da285f00e2975ce8d
Sarmad Nashat <sarmad@sarosmbp.local>
2019-03-29 21:20:39 +0300

    renamed, deleted and added some files

--- odd_even.py
+++ odd_even.py
@@ -2,9 +2,9 @@ def main():
 
     x = int(input("enter number: "))
     if x%2==0:
-        print("square")
+        print("it is an even number")
     else:
-        print("see you")
+        print("it is an odd number")
 
 if __name__ == "__main__":
     main()
