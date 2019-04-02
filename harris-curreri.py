ad554661896a5bded0685773e3e85c5cdb13a8ab
Sarmad Nashat <sarmad@sarosmbp.local>
2019-04-02 23:20:45 +0300

    some improvements

--- harris-curreri.py
+++ harris-curreri.py
@@ -1,13 +1,12 @@
 # This program calculates total energy requirement in burn patients
-# It uses the two widely used formula; Curreri and Harris-Benedict
-
+# It uses the two widely applied formula; Curreri and Harris-Benedict
 
 def main():
 
     class Patient:
 
         def __init__(self):
-            self.sex = ["m", "f"]
+            self.sex = 0
             self.age = 0
             self.wt = 0
             self.ht = 0
@@ -18,9 +17,11 @@ def main():
             if self.age < 16:
                 self.kcal = 60 * self.wt + 35 * self.tsba
                 return self.kcal
-            else:
+            elif self.age > 16:
                 self.kcal = 25 * self.wt + 40 * self.tsba
                 return self.kcal
+            else:
+                pass
 
         def harris(self):
             if self.sex == "m":
@@ -31,16 +32,37 @@ def main():
                 return self.kcal
             else:
                 pass
-    
+
+        def average(self):
+            return (self.curreri() + self.harris()) / 2
+
+# A simple interface
+    print("\n")
+    print("=====================================================")
+    print("||                                                 ||")
+    print("||          Energy Requirement Calculator          ||")
+    print("||                                                 ||")
+    print("=====================================================")
+# Ask the user to input the patient parameters    
     new_ptn = Patient()
-    new_ptn.sex = str(input("Patient gender (m/f): "))
+    new_ptn.sex = str(input("Patient gender(m/f): "))
     new_ptn.age = int(input("Age(years): "))
     new_ptn.wt = int(input("Weight(kg): "))
     new_ptn.ht = int(input("Height(cm): "))
     new_ptn.tsba = int(input("TSBA(%): "))
 
-    print(new_ptn.curreri())
-    print(new_ptn.harris())
+# This outputs the Curreri formula
+    print("\nCurreri Energy Requirement")
+    print(str(new_ptn.curreri()) + " kcal")
+
+# This outputs the Harris-Benedict formula
+    print("\nHarris-Benedict Basal Energy Expenditure")
+    print(str(new_ptn.harris()) + " kcal")
+
+# This outputs the average of the two systems
+    print("\nTotal Daily Energy Requirement (Avg.)")
+    print(str(new_ptn.average()) + " kcal")
+    print("\n")
 
 if __name__ == "__main__":
     main()
